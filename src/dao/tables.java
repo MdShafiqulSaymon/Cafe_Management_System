096b037362a492389a3409ed80f0258253d27cf3
MD.SHAFIQUL HASAN SAYMON <mdshafiqulsaymon@gmail.com>
2023-01-22 21:49:16 +0600

    here we Done the Category page and add category , delete and Home page Design and connection

--- src/dao/tables.java
+++ src/dao/tables.java
@@ -14,9 +14,10 @@ public class tables {
         try{
             String userTable = "create table user(id int AUTO_INCREMENT primary key, name varchar(200),email varchar(200),mobileNumber varchar(10),address varchar(200),password varchar(200),securityQuestion varchar(200),answer varchar(200),status varchar(20),UNIQUE(email))";
             String adminDetails = "insert into user(name,email,mobileNumber,address,password,securityQuestion,answer,status) values('Admin','admin@gmail.com','1234567890','Bangladesh','admin','What primary school did you attend?','ABC','true')";
-            
+            String categoryTable = "create table category (id int AUTO_INCREMENT primary key,name varchar(200))";
             DbOperations.setDataOrDelete(userTable, "User Table Created Successfully");
             DbOperations.setDataOrDelete(adminDetails, "Admin details Added Successfully");
+            DbOperations.setDataOrDelete(categoryTable, "Category Table Created Successfully");
         }
         catch(Exception e){
             JOptionPane.showMessageDialog(null, e);
