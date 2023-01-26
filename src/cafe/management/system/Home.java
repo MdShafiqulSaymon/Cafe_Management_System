3ceefee33fbec198e767c91bc48cadc0c8e068f7
MD.SHAFIQUL HASAN SAYMON <mdshafiqulsaymon@gmail.com>
2023-01-27 00:01:14 +0600

    AddNewProduct Page Complete

--- src/cafe/management/system/Home.java
+++ src/cafe/management/system/Home.java
@@ -134,6 +134,7 @@ public class Home extends javax.swing.JFrame {
 
     private void btnNewProductActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnNewProductActionPerformed
         // TODO add your handling code here:
+        new AddNewProduct().setVisible(true);
     }//GEN-LAST:event_btnNewProductActionPerformed
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
